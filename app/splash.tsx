0b9fcdd858cf69c92d266f907ca79cefcd2fdbb2
shay-veev <113917630+shay-veev@users.noreply.github.com>
2025-07-28 13:45:35 +0300

    Updated splash.tsx

--- app/splash.tsx
+++ app/splash.tsx
@@ -44,7 +44,7 @@ export default function SplashScreen() {
       ]).start(() => {
         router.replace('/(tabs)');
       });
-    }, 5000);
+    }, 10000);
 
     return () => clearTimeout(timer);
   }, [router]);
